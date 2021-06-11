9543e16e106f54f54664380684dbe2a389f932b6
DylanJaramillo <dyljar0123@hotmail.com>
2021-06-11 10:32:58 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Que mas mija");
+            Console.Write("Todo posi mi so!");
+        }
+    }
+}
